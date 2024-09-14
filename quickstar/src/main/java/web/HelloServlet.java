fb6d4bdc53dab270f0e2bf0ec1c7eef267d176db
Zdc2Pww <3481126848@qq.com>
2024-09-14 16:45:35 +0800

    quickstar

---
+++ quickstar/src/main/java/web/HelloServlet.java
@@ -0,0 +1,24 @@
+package web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + "庄冬晨2305271407" + "</h1>");
+        out.println("</body></html>");
+        System.out.println("庄冬晨");
+    }
+}
